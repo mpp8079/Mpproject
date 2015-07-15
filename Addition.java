3952788856940f67aebeaf2fe2f636e1264213c5
mpp8079 <mpp8079@gmail.com>
2015-07-14 22:05:59 -0500

    Log4jDemo
    Log4Demo

---
+++ Addition.java
@@ -0,0 +1,23 @@
+package mathMethod;
+
+import org.apache.log4j.Logger;
+
+public class Addition {
+	
+	final static Logger log = Logger.getLogger(Addition.class);
+	
+	public static void main(String[]args){
+		int a = 15;
+		int b = 20;
+		int d =a+b;
+	
+		log.trace("Trace Message");
+		log.debug("Debug Message");
+		log.info("Info Message");
+		log.warn("Warn Message");
+		log.error("Error Message");
+	}
+	
+	
+
+}
