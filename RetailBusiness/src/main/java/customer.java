84a98b67353e99d8295b276584902c2f776e8040
manish <mpp8079@gmail.com>
2015-08-04 22:42:54 -0500

    Signed-off-by: manish <mpp8079@gmail.com>

---
+++ RetailBusiness/src/main/java/customer.java
@@ -0,0 +1,20 @@
+
+class customer {
+	
+	private int customerId;
+	private String customerName;
+	
+	public void setcustomerId(int id){
+		customerId= id;
+	}
+	public void setcustomerName(String name){
+		customerName= name;
+	}
+	
+	public String getCustomerName(){
+		return customerName;
+	}
+		
+	}
+
+
